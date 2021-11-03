e9147bd2802da5630bd28e1059c8f7632baa5aca
dodotronix <pacnerpetr@volny.cz>
2021-11-03 23:42:24 +0100

    refactoring of constraints generation script

--- scripts/assemble_constraints.py
+++ scripts/assemble_constraints.py
@@ -1,104 +1,295 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import csv
+import os
+import sys
+import re
+import logging as log
 
-#user configuration
-dev = "ME-XU5-4CG/4EV/5EV-G1"
-non_dep_pinout ="../pinout/xu5_module/Mercury_XU5-R1_FPGA_Pinout.csv"
-var_dep_pinout ="../pinout/xu5_module/Mercury_XU5-R1_FPGA_Pinout_Assembly_Variants.csv"
-pin_mapping_mcoi_a ="../pinout/mcoi/pin_conn_a.csv"
-pin_mapping_mcoi_b ="../pinout/mcoi/pin_conn_b.csv"
+from io import StringIO
+from argparse import ArgumentParser, ArgumentError
 
-with open(non_dep_pinout, newline='') as csvfile:
-    f = csv.reader(csvfile, delimiter=',')
-    dep = []
-    var_dep = []
-    for i in f: 
-        if(len(i) == 4) and ("*") in i[3]:
-            var_dep.append(i)
-        if(len(i) == 4) and ("J80") in i[3]:
-            dep.append(i)
+#submit file names as script arguments
+parser = ArgumentParser(description = 'Generates constraints files for ' 
+                                      'MCOI XU5 platform')
 
-with open(var_dep_pinout, newline='') as csvfile:
-    f = csv.reader(csvfile, delimiter=',')
-    var_assem_raw = [i for i in f if(len(i) == 5 and i[4])]
-    
-with open(pin_mapping_mcoi_a, newline='') as csvfile:
-    f = csv.reader(csvfile, delimiter=',')
-    pinmap_a = [i[0:3] for i in f][3:]
+parser.add_argument('-d', '--device',
+        metavar = 'STRING',
+        type = str,
+        help = 'Full name of the device taken from Enclustra catalog')
+parser.add_argument('-ap', '--apinout',
+        metavar = 'FILE',
+        type = str,
+        help = 'Pinout of connector A as is specified in Enclustra XU5 schematics')
+parser.add_argument('-bp', '--bpinout',
+        metavar = 'FILE',
+        type = str,
+        help = 'Pinout of connector B as is specified in Enclustra XU5 schematics')
+parser.add_argument('-va','--vendor_assembly',
+        metavar = 'FILE',
+        type = str,
+        help = 'Enclustra csv file defining pinouts for different module versions')
+parser.add_argument('-vp', '--vendor_pinout',
+        metavar = 'FILE',
+        type = str,
+        help = 'Enclustra csv file with pinout of the specific module')
+parser.add_argument('-o', '--output_directory',
+        metavar = 'PATH',
+        type = str,
+        help = 'Output directory for constraints files')
+parser.add_argument('-c', '--config',
+        metavar = 'FILE',
+        required=False,
+        type = str,
+        help = 'File with additional configs for module pins')
+parser.add_argument('-io', '--iostandard',
+        metavar = 'STRING',
+        type = str,
+        help = 'Default IO standard which will be used if the pins ' 
+        'not defined in the config file')
 
-with open(pin_mapping_mcoi_b, newline='') as csvfile:
-    f = csv.reader(csvfile, delimiter=',')
-    pinmap_b = [i[0:3] for i in f][3:]
+args = vars(parser.parse_args())
+
+dev = args['device']
+non_dep_pinout = args['vendor_pinout']
+var_dep_pinout = args['vendor_assembly']
+pin_mapping_mcoi_a = args['apinout']
+pin_mapping_mcoi_b = args['bpinout']
+destination_folder = args['output_directory']
+board_config = args['config']
+default_iostandard = args["iostandard"]
+
+# if the input files change, you can just modify the regex
+# this regex counts on that the the file has Pin Designator column and 168 pins
+conns = re.compile(r'''Pin\s+Designator.*168.*\n''',
+                   re.MULTILINE | re.VERBOSE | re.DOTALL)
+
+# Enclustra files are already modified. The files are originaly in xlsx
+# so first they were exported to csv and the redundant footers were removed 
+get_all = re.compile(r'''.*''',
+                     re.MULTILINE | re.VERBOSE | re.DOTALL)
+
+def create_dict(file, regex_pattern):
+    with open(file, "r", newline='') as f:
+        #create raw one piece string
+        raw_txt_data = ''.join(f.readlines()) 
+        # there should be just one match so there fore group(0)
+        try:
+            pins_a = regex_pattern.search(raw_txt_data).group(0) 
+        except:
+            log.error('File: {0} has probably '
+                      'different structure than expected\n'.format(file))
+        dict_iter = csv.DictReader(StringIO(pins_a),
+                                   skipinitialspace=True, 
+                                   delimiter=',')
+        return [i for i in dict_iter]
+
+# read data and create list of dictionaries
+conn_a = create_dict(pin_mapping_mcoi_a, conns)
+conn_b = create_dict(pin_mapping_mcoi_b, conns)
+variants = create_dict(non_dep_pinout, get_all)
+assembly = create_dict(var_dep_pinout, get_all)
+config = create_dict(board_config, get_all)
 
 
-# filter just the pins for selected device in configuration
-col = var_assem_raw[0].index(dev)
-var_assem = [[i[0], i[1], i[col]] for i in var_assem_raw][2:]
+dep_t = [i for i in variants if("J80" in i["Connector"])]
+dep_var_t = [i for i in variants if(i["Connector"] == "*")]
 
-for i in var_assem:
-    for n in var_dep:
-        if(i[2] == n[0]):
-            con = i[1].split("-")
-            if(con[0] == "A"):
-                n[3] = "J800." + con[1] 
-            else:
-                n[3] = "J801." + con[1]
+#list of signals on module (name from enclustra xlsx) 
+signal_on_module = [i["Signal on module"] for i in dep_var_t] 
+# create map of indexes for variants dependent connections
+pin_inx = [signal_on_module.index(i[dev]) for i in assembly]
+# replace the asterisks 
+for n,i in enumerate(pin_inx):
+    pin_number = dep_var_t[i]["FPGA pin number"]
+    # This variable shows the pcb connection on the particular variant
+    platform_con = assembly[n]["Module Variant"] 
+    # this reads data from assembly file and according to whether it is with
+    # prefix A or B it adds the value to the places with * based on the
+    # "pin_inx" and add the prefix J800 or J801 
+    con_num = platform_con.split("-")[1] 
+    dep_var_t[i]["Connector"] = ("J800.{0}".format(con_num) 
+                                 if("A" in platform_con) 
+                                 else "J801.{0}".format(con_num))
+    #remove all unconnected routes with *
+    rm_asterisk = [i for i in dep_var_t if i["Connector"] != "*"]
+    # full configuration of the chosen module
+    full_configuratin = rm_asterisk + dep_t
 
-var_dep = [x for x in var_dep if x[3] != "*"]
+# split the pin set in two (connector A and B as in the schematics)
+connector_a = [i for i in full_configuratin if("J800." in i["Connector"])] 
+connector_b = [i for i in full_configuratin if("J801." in i["Connector"])] 
 
-def hasNumbers(inputString):
-    return any(char.isdigit() for char in inputString)
+# sort for easier assignment to Altium pins
+# connector_a.sort(key=lambda x : x["Connector"])
+# connector_b.sort(key=lambda x : x["Connector"])
 
-def pin_stamp_const(pin, name):
-    return "set_property PACKAGE_PIN {0} [get_ports {1}]\n".format(pin, name)
+# take names from Altium files and rename the pins on enclustra module
+def assign_pin_names(connector, altium_pins, parsed_list):
+    for i in connector:
+        # read pin number and make it integer
+        # substracting one at the end is 
+        # because of the list zero index
+        inx = int(i["Connector"].split(".")[1])-1
+        # first check if the pin is used otherwise 
+        # exclude it from pin set
+        if(altium_pins[inx]["Net Name"] != ""):
+            i["Signal on module"] = altium_pins[inx]["Net Name"]
+            parsed_list.append(i)
 
-def pin_stamp_const_array(pin, name, num):
-    return ("set_property PACKAGE_PIN {0} [get_ports {{{1}[{2}]}}]\n".format(pin, name, num))
+# assign pcb pin names to module pins and remove unconnected pins
+new_connector_a = []; new_connector_b = []
+assign_pin_names(connector_a, conn_a, new_connector_a)
+assign_pin_names(connector_b, conn_b, new_connector_b)
 
-#join all
-final = var_dep + dep 
+# working pin set
+pins_complete = new_connector_a + new_connector_b
 
-# replace J800 or J801 with connector pin number
-final_a = []
-final_b = []
-for x in final:
-    if "J800." in x[3]:
-        x[3] = int(x[3].replace("J800.", ""))
-        final_a.append(x)
-    elif "J801." in x[3]:
-        x[3] = int(x[3].replace("J801.", ""))
-        final_b.append(x)
+# create dictionary sorting out pin vectors in groups
+# dictionary key is name of the pin
+groups = {}
+for i in pins_complete:
+    # if there is a number at the and of the pin name 
+    # it means it's an array
+    raw_name = i["Signal on module"].rsplit("_", 1)
+    name = raw_name[0]
+    # not all pins have number or "_" sign at the end of its name
+    try:
+        num = raw_name[1]
+    except IndexError:
+        num = "x"
+    # setdefault is there because the list in dictionary 
+    # doesn't have to exist yet
+    groups.setdefault(name.lower() if str.isnumeric(num) 
+            else i["Signal on module"].lower(), []).append(i)
 
-#sort arrays
-final_a.sort(key=lambda x : x[3])
-final_b.sort(key=lambda x : x[3])
+# grouping of pins according to content in config file
+new_groupping = {}
+for i in config:  
+    # find all occurences of names in the config file 
+    pin_search = [n for g in groups if(i["pin"] in g) for n in groups[g]]
+    # add custom settings from config file to the pin dictionary
+    for p in pin_search:
+        p.update(i)
+    new_groupping.setdefault(i["group"], []).append(pin_search)
+    # remove all keys with the occurence of "i["pin"]" in groups
+    groups = {k : v for k,v in groups.items() if(i["pin"] not in k)}
 
-# rename pins on connector A according to altium
-for i in final_a:
-    i[0] = pinmap_a[i[3]-1][1]
+def get_set_property_standard(pin, name):
+    return ("set_property PACKAGE_PIN {0} "
+           "[get_ports {1}]\n".format(pin, name))
 
-# rename pins on connector B according to altium
-for i in final_b:
-    i[0] = pinmap_b[i[3]-1][1]
+def get_set_property_vector(pin, name, num):
+    return ("set_property PACKAGE_PIN {0} "
+            "[get_ports {{{1}[{2}]}}]\n".format(pin, name, num))
 
-final = final_a + final_b
-final.sort(key=lambda x : x[0])
+def get_group_comment(group_name, iostandard):
+    return ("# MCOI XU5 PINGROUP: {0}; "
+            "IOSTANDARD: {1}".format(group_name.upper(), 
+                                       iostandard.upper()))
 
-constraints = ""
-for i in final:
-    if(i[0]):
-        raw_name = i[0].rsplit("_", 1)
-        num = raw_name[-1]
-        name = raw_name[0]
-        if(str.isnumeric(num)):
-            # TODO probably sort out the groups and sort theme 
-            constraints += pin_stamp_const_array(i[1], name.lower(), num)
-        else:
-            constraints += pin_stamp_const(i[1], i[0].lower())
+def get_create_clock(name, pin, frequency):
+    # multiplying by 1000 is because units are in "ns"
+    return ("create_clock -period {0:.3f} -name "
+            " {1} [get_ports {2}]\n".format((1000/frequency), name, pin))
 
-# generate constraint file
-with open('constraints_mcoi_io.xdc', 'w') as f:
-    f.write(constraints)
+def get_iostandard_stamp(iostandard, pin):
+    return ("set_property IOSTANDARD {0} [get_ports "
+            "{{{1}}}]\n".format(iostandard.upper(), pin))
+
+def create_clock_stamp(clk_list):
+    # pin key in dictionary is user identificator for pins of group
+    stamp = "{0}".format(get_group_comment(clk_list[0]["pin"], 
+             clk_list[0]["iostandard"]))
+
+    for i in clk_list:
+        name = i["Signal on module"].lower()
+        user_name = i["pin"]
+        pin = i["FPGA pin number"]
+        iostandard = i["iostandard"]
+        frequency = int(i["freq[Mhz]"])
+        stamp = "{0}\n{1}{2}".format(stamp, 
+                 get_set_property_standard(pin, name),
+                 get_iostandard_stamp(iostandard, name))
+    return "{0}\n{1}\n".format(stamp, get_create_clock(
+            user_name, name, frequency))
+
+def create_clock_constr(clk_list):
+    stamp = ""
+    for i in clk_list:
+        stamp = "{0}{1}".format(stamp, create_clock_stamp(i))
+    return stamp
+
+def create_gpio_stamp_vector(gpio_list):
+    stamp = ""
+    # sort array using suffix number 
+    gpio_list_sorted = sorted(gpio_list, key=lambda x: 
+            int(x["Signal on module"].rsplit("_", 1)[1]))  
+    for i in gpio_list_sorted:
+        raw_name = i["Signal on module"].rsplit("_", 1)
+        num = int(raw_name[1]) 
+        user_name = raw_name[0].lower()
+        pin = i["FPGA pin number"]
+        iostandard = (i["iostandard"] if("iostandard" in i) 
+                     else default_iostandard) 
+        stamp = "{0}{1}".format(stamp, 
+                 get_set_property_vector(pin, user_name, num))
+    return "{0}{1}\n".format(stamp, get_iostandard_stamp(
+           iostandard, user_name))
+
+def create_gpio_stamp_standard(gpio_list):
+    stamp = ""
+    # gpio_list for one pin is one element list
+    # therefore there is the zero index
+    gpio = gpio_list[0]
+    pin = gpio["FPGA pin number"]
+    user_name = gpio["Signal on module"].lower()
+    iostandard = (gpio["iostandard"] if("iostandard" in i) 
+                 else default_iostandard) 
+    return "{0}{1}".format(get_set_property_standard(pin, user_name),
+            get_iostandard_stamp(iostandard, user_name))
+
+#TODO generate gpios and groups
+def create_gpios_constr(gpio_list):
+        stamp = ""
+        for i in gpio_list:
+            stamp_type = (create_gpio_stamp_vector(i) if (len(i) > 1) 
+                         else create_gpio_stamp_standard(i)) 
+            stamp = "{0}{1}".format(stamp, stamp_type)
+        return stamp
+
+# create strings which will be written to constraints files
+clock_constr = ""
+gpios_constr = ""
+for i in new_groupping:
+    if(i == "clk"):
+        clock_constr = "# CLOCK CONFIGURATION CONSTRAINTS"
+        clock_constr = "{0}\n\n{1}".format(clock_constr,
+                        create_clock_constr(new_groupping[i]))
+    else:
+        gpios_constr = "{0}# PIN GROUPPING: {1}".format(gpios_constr,
+                       i.upper())
+        gpios_constr = "{0}\n{1}".format(gpios_constr,
+                       create_gpios_constr(new_groupping[i]))
+
+for i in groups:
+        gpios_constr = "{0}# PIN GROUPPING: {1}".format(gpios_constr,
+                       i.upper())
+        # somehowe the groups[i] has to be encapsulated into list
+        # because the creat_gpios_constr takes list as an argument 
+        gpios_constr = "{0}\n{1}".format(gpios_constr,
+                       create_gpios_constr([groups[i]]))
+
+# generate clock constraint file
+file_path = os.path.join(destination_folder, 
+                         'constraints_mcoi_clk.xdc') 
+with open(file_path, 'w') as f:
+    f.write(clock_constr)
     f.close()
 
+# generate gpio constraint file
+file_path = os.path.join(destination_folder, 
+                         'constraints_mcoi_io.xdc') 
+with open(file_path, 'w') as f:
+    f.write(gpios_constr)
+    f.close()
