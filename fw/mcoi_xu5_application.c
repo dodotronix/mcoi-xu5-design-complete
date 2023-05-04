eb1133d63a2cc6c37b834992fee1c68155d8ccf8
Petr Pacner <petr.pacner@cern.ch>
2023-05-04 16:59:25 +0200

    i2c configuration for external pll - not working yet

--- fw/mcoi_xu5_application.c
+++ fw/mcoi_xu5_application.c
@@ -1,30 +1,8 @@
 /*
-* This licence is copyright (c) CERN 2021, and the text of this licence is
-* licensed as a whole under a Creative Commons Attribution-NoDerivatives 4.0
-* International Licence, (CC BY-ND 4.0), available at
-* https://creativecommons.org/licenses/by-nd/4.0/
-* As a special exception to that licence, CERN expressly permits faithful
-* translations of the entire licence into any language, and subsequent public
-* display and distribution, provided that the resulting translation (which may
-* include an attribution to the translator) is released under CC BY-ND 4.0
-* attributing CERN as the copyright holder, and providing a link to
-* the original licence text at
-* https://ohwr.org/cern_ohl_w_v2.txt. This paragraph (translated
-* appropriately) must be included when copying or translating the licence. You
-* must make it clear (by at least including this paragraph) that the
-* translation is unofficial and that the original text of the licence
-* in the English language as released by CERN is definitive. You may
-* also extract parts of the licence text (both in the original and in
-* faithful translation) and copy, publicly display and distribute them
-* for the purposes of education, training, providing legal advice,
-* reviewing and critiquing the licence provided that you attribute
-* CERN as above and also provided that such extracted parts do not form part of
-* any document intended to have legal effect.
-
-* created on 01 December 2021
-* Author: Petr Pacner/DB
-* Email:  petr.pacner@cern.ch
-*/
+ *
+ * https://docs.xilinx.com/r/en-US/ug1085-zynq-ultrascale-trm/Introduction?tocId=woBbxhIOVledjl_YLyd~cQ
+ * https://docs.xilinx.com/r/en-US/oslib_rm/BSP-Configuration-Settings?tocId=wMpjv8~9UySetA6BBWH~9Q
+ */
 
 #include <stdio.h>
 #include "platform.h"
@@ -33,6 +11,7 @@
 #include "xparameters.h"
 #include "sleep.h"
 #include "xiicps.h"
+#include "config_120mhz.h"
 
 void show_iic_setup(XIicPs *InstancePtr) {
     printf("Device ID: %x\n", InstancePtr->Config.DeviceId);
@@ -54,26 +33,26 @@ void show_iic_setup(XIicPs *InstancePtr) {
     printf("Is 10Bit Addr: %lx\n", InstancePtr->Is10BitAddr);
 }
 
-
 #define IIC_DEVICE_ID		XPAR_XIICPS_0_DEVICE_ID
-
 #define IIC_SLAVE_ADDR		0x70
 #define IIC_SCLK_RATE		100000
-#define TEST_BUFFER_SIZE	1
 
-int IicPsMasterPolledTest(u16 DeviceId);
+int initialize_iic(u16 DeviceId, XIicPs *iic_dev);
+u8 read_byte_data(XIicPs *iic_dev, u16 addr, u8 reg);
+void write_byte_data(XIicPs *iic_dev, u16 addr, u8 reg, u8 val);
+void set_bits(XIicPs *iic_dev, u8 reg, u8 val);
+void clear_bits(XIicPs *iic_dev, u8 reg, u8 val);
+void copy_bits(XIicPs *iic_dev, u8 reg0, u8 reg1, u8 mask);
+void validate_value(XIicPs *iic_dev, u8 reg, u8 val);
+void write_config_map(XIicPs *iic_dev, __Reg_Data *map, u32 map_lengh);
+int initialize_clock(u16 DeviceId);
 
-XIicPs Iic;
-
-u8 SendBuffer[TEST_BUFFER_SIZE];    /**< Buffer for Transmitting Data */
-u8 RecvBuffer[TEST_BUFFER_SIZE];    /**< Buffer for Receiving Data */
-
+u8 SendBuffer[2];
+u8 RecvBuffer[1];
 
 int main(void)
 {
-	int Status;
     char test[20];
-
     printf("waiting for an input:");
     scanf("%20s", test);
     printf("\n");
@@ -82,65 +61,158 @@ int main(void)
     }
     printf("\n");
 
-    xil_printf("IIC Master Polled Example Test \r\n");
+    printf("Initialize External PLL\r\n");
+    initialize_clock(IIC_DEVICE_ID);
 
-    Status = IicPsMasterPolledTest(IIC_DEVICE_ID);
-    if (Status != XST_SUCCESS) {
-        xil_printf("IIC Master Polled Example Test Failed\r\n");
+    return 0;
+}
+
+void set_bits(XIicPs *iic_dev, u8 reg, u8 val) {
+    u8 tmp, current_reg;
+    current_reg = read_byte_data(iic_dev,DEVICE_ADDRESS, reg);
+    tmp = curre_reg | val;
+    write_byte_data(iic_dev, DEVICE_ADDRESS, reg, tmp);
+    tmp = read_byte_data(DEVICE_ADDRESS, reg);
+    printf("[DEBUG]: set bits (%x ---> %x)\n", current_reg, tmp);
+}
+
+void clear_bits(XIicPs *iic_dev, u8 reg, u8 val) {
+    u8 tmp, current_reg;
+    current_reg = read_byte_data(iic_dev, DEVICE_ADDRESS, reg);
+    tmp = curre_reg & ~val;
+    write_byte_data(iic_dev, DEVICE_ADDRESS, reg, tmp);
+    tmp = read_byte_data(iic_dev, DEVICE_ADDRESS, reg);
+    printf("[DEBUG]: cleared bits (%x ---> %x)\n", current_reg, tmp);
+}
+
+void copy_bits(XIicPs *iic_dev, u8 reg0, u8 reg1, u8 mask) {
+    u8 tmp, current_reg0, current_reg1;
+    current_reg0 = read_byte_data(iic_dev, DEVICE_ADDRESS, reg0);
+    current_reg1 = read_byte_data(iic_dev, DEVICE_ADDRESS, reg1);
+    
+    tmp = current_reg1 | (current_reg0 & mask);
+    write_byte_data(iic_dev, DEVICE_ADDRESS, reg1, tmp);
+    printf("[DEBUG]: (%x ---> %x)\n", current_reg0, current_reg1, tmp);
+}
+
+void validate_value(XIicPs *iic_dev, u8 reg, u8 mask, u8 val) {
+    printf("[DEBUG]: validating value in register %x (desired value %x)\n", reg, val);
+    while(not((read_byte_data(iic_dev, DEVICE_ADDRESS, reg) & mask) == val));
+}
+
+void write_config_map(XIicPs *iic_dev, __Reg_Data *map, u32 map_lengh) {
+    //TODO config map
+    for(u32 i=0; i<map_length; ++i) {
+        if(imask == 0xff){
+            write_byte_data(DEVICE_ADDRESS, ireg, ival)
+
+            log.info('reg: {0}, _ => {1} | reg value: {2}'.format(ireg, 
+            ival, bus.read_byte_data(DEVICE_ADDRESS, ireg)))
+        }
+        else {
+            current_reg = bus.read_byte_data(DEVICE_ADDRESS, ireg)
+            clear_cur_val = (current_reg & ~imask)
+            clear_new_val = ival & imask
+            combined = clear_cur_val | clear_new_val
+            bus.write_byte_data(DEVICE_ADDRESS, ireg, combined)
+
+            printf('reg: {0}, {1} => {2} | reg value: {3}'.format(ireg, 
+            current_reg, combined, bus.read_byte_data(DEVICE_ADDRESS, ireg)))
+        }
+    }
+}
+
+void write_byte_data(XIicPs *iic_dev, u16 addr, u8 reg, u8 val) {
+    SendBuffer[0] = reg;
+    SendBuffer[1] = val;
+    int Status;
+
+    // Wait for the line to be IDLE
+	while (XIicPs_BusIsBusy(iic_dev));
+	Status = XIicPs_MasterSendPolled(iic_dev, SendBuffer, 2, addr);
+	if (Status) printf("Failed Reading from the I2C\n");
+}
+
+u8 read_byte_data(XIicPs *iic_dev, u16 addr, u8 reg) {
+
+    int Status;
+    SendBuffer[0] = reg;
+    // wait until data line is idle again 
+	while (XIicPs_BusIsBusy(iic_dev));
+	Status = XIicPs_MasterSendPolled(iic_dev, SendBuffer, 1, addr);
+
+	while (XIicPs_BusIsBusy(iic_dev));
+	Status = XIicPs_MasterRecvPolled(iic_dev, RecvBuffer, 1, addr);
+	if (Status) printf("Failed Reading from the I2C\n");
+
+    return RecvBuffer[0];
+}
+
+int initialize_iic(u16 DeviceId, XIicPs *Iic) {
+
+	int Status;
+	XIicPs_Config *Config;
+
+	Config = XIicPs_LookupConfig(DeviceId);
+	if (NULL == Config) return XST_FAILURE;
+
+	Status = XIicPs_CfgInitialize(Iic, Config, Config->BaseAddress);
+	if (Status != XST_SUCCESS) return XST_FAILURE;
+
+	Status = XIicPs_SelfTest(Iic);
+	if (Status) return XST_FAILURE;
+
+	XIicPs_SetSClk(Iic, IIC_SCLK_RATE);
+    return XST_SUCCESS;
+}
+
+int initialize_clock(u16 DeviceId) {
+    
+    XIicPs Iic;
+
+    if(initialize_iic(DeviceId, &Iic)) { 
+        printf("I2C INITIALIZATION FAILED");
         return XST_FAILURE;
     }
 
-    xil_printf("Successfully ran IIC Master Polled Example Test\r\n");
+    write_byte_data(&Iic, IIC_SLAVE_ADDR, 0x31, 0x10);
+    printf("Read vaule: %u",read_byte_data(&Iic, IIC_SLAVE_ADDR,  0x31));
+
+    /* set_bits(&Iic, 230, 0x10) //disable outputs
+
+    printf("lol pause");
+    set_bits(&Iic, 241, 0x80); //pause lol
+
+    printf("writing config");
+    write_config_map(&Iic, code_Reg_Store, NUM_REGS_MAX);
+
+    printf("validating input clock ...");
+    validate_value(&Iic, 218, 0x04, 0x00);
+    printf("ok");
+
+    clear_bits(&Iic, 49, 0x80); //configure pll for locking
+    set_bits(&Iic, 246, 0x02); //initiate locking of ppl
+    
+    usleep(0.025); //wait 25ms
+
+    clear_bits(&Iic, 241, 0x80); //restart lol
+    set_bits(&Iic, 241, 0x65);
+    
+    printf("waiting for locking pll ...");
+    validate_value(&Iic, 218, 0x15, 0x00);
+    printf("ok"&Iic, )//
+
+    printf("copying registers");
+    copy_bits(&Iic, 237, 47, 0x03);
+    copy_bits(&Iic, 236, 46, 0xff);
+    copy_bits(&Iic, 235, 45, 0xff);
+
+    set_bits(&Iic, 47, 0x14);
+    set_bits(&Iic, 49, 0x80); //set pll to use fcal
+
+    printf("enabling outputs");
+    clear_bits(&Iic, 230, 0x10); //enable outputs
+    printf("done"); */
+
     return XST_SUCCESS;
-
-}
-
-int IicPsMasterPolledTest(u16 DeviceId)
-{
-	int Status;
-	XIicPs_Config *Config;
-	int Index;
-
-	Config = XIicPs_LookupConfig(DeviceId);
-	if (NULL == Config) {
-		return XST_FAILURE;
-	}
-
-	Status = XIicPs_CfgInitialize(&Iic, Config, Config->BaseAddress);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-	Status = XIicPs_SelfTest(&Iic);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-	XIicPs_SetSClk(&Iic, IIC_SCLK_RATE);
-
-	for (Index = 0; Index < TEST_BUFFER_SIZE; Index++) {
-		SendBuffer[Index] = (Index % TEST_BUFFER_SIZE);
-		RecvBuffer[Index] = 0;
-	}
-
-	Status = XIicPs_MasterSendPolled(&Iic, SendBuffer,
-			 TEST_BUFFER_SIZE, IIC_SLAVE_ADDR);
-
-    printf("Status after writing data to FIFO: %i", Status);
-
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-    show_iic_setup(&Iic);
-
-	while (XIicPs_BusIsBusy(&Iic));
-
-	Status = XIicPs_MasterRecvPolled(&Iic, RecvBuffer,
-			  TEST_BUFFER_SIZE, IIC_SLAVE_ADDR);
-	if (Status != XST_SUCCESS) {
-		return XST_FAILURE;
-	}
-
-	return XST_SUCCESS;
 }
