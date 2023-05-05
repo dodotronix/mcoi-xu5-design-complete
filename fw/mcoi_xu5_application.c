3cc8458e55cf7b5a72979eb8e02258cfd83db8b8
Petr Pacner <petr.pacner@cern.ch>
2023-05-05 17:07:15 +0200

    working prototype of the si5338b pll loading program

--- fw/mcoi_xu5_application.c
+++ fw/mcoi_xu5_application.c
@@ -11,7 +11,8 @@
 #include "xparameters.h"
 #include "sleep.h"
 #include "xiicps.h"
-#include "config_120mhz.h"
+// #include "config_120mhz.h"
+#include "config_400khz.h"
 
 void show_iic_setup(XIicPs *InstancePtr) {
     printf("Device ID: %x\n", InstancePtr->Config.DeviceId);
@@ -43,8 +44,8 @@ void write_byte_data(XIicPs *iic_dev, u16 addr, u8 reg, u8 val);
 void set_bits(XIicPs *iic_dev, u8 reg, u8 val);
 void clear_bits(XIicPs *iic_dev, u8 reg, u8 val);
 void copy_bits(XIicPs *iic_dev, u8 reg0, u8 reg1, u8 mask);
-void validate_value(XIicPs *iic_dev, u8 reg, u8 val);
-void write_config_map(XIicPs *iic_dev, __Reg_Data *map, u32 map_lengh);
+void validate_value(XIicPs *iic_dev, u8 reg, u8 mask, u8 val);
+void write_config_map(XIicPs *iic_dev, __Reg_Data const *map, u32 map_length);
 int initialize_clock(u16 DeviceId);
 
 u8 SendBuffer[2];
@@ -69,55 +70,57 @@ int main(void)
 
 void set_bits(XIicPs *iic_dev, u8 reg, u8 val) {
     u8 tmp, current_reg;
-    current_reg = read_byte_data(iic_dev,DEVICE_ADDRESS, reg);
-    tmp = curre_reg | val;
-    write_byte_data(iic_dev, DEVICE_ADDRESS, reg, tmp);
-    tmp = read_byte_data(DEVICE_ADDRESS, reg);
-    printf("[DEBUG]: set bits (%x ---> %x)\n", current_reg, tmp);
+    current_reg = read_byte_data(iic_dev, IIC_SLAVE_ADDR, reg);
+    tmp = current_reg | val;
+    write_byte_data(iic_dev, IIC_SLAVE_ADDR, reg, tmp);
+    current_reg = read_byte_data(iic_dev, IIC_SLAVE_ADDR, reg);
+    printf("[DEBUG]: addr %i; written %x ---> read %x\n", reg, tmp, current_reg);
 }
 
 void clear_bits(XIicPs *iic_dev, u8 reg, u8 val) {
     u8 tmp, current_reg;
-    current_reg = read_byte_data(iic_dev, DEVICE_ADDRESS, reg);
-    tmp = curre_reg & ~val;
-    write_byte_data(iic_dev, DEVICE_ADDRESS, reg, tmp);
-    tmp = read_byte_data(iic_dev, DEVICE_ADDRESS, reg);
-    printf("[DEBUG]: cleared bits (%x ---> %x)\n", current_reg, tmp);
+    current_reg = read_byte_data(iic_dev, IIC_SLAVE_ADDR, reg);
+    tmp = current_reg & ~val;
+    write_byte_data(iic_dev, IIC_SLAVE_ADDR, reg, tmp);
+    current_reg = read_byte_data(iic_dev, IIC_SLAVE_ADDR, reg);
+    printf("[DEBUG]: addr %i; written %x ---> read %x\n", reg, tmp, current_reg);
 }
 
 void copy_bits(XIicPs *iic_dev, u8 reg0, u8 reg1, u8 mask) {
     u8 tmp, current_reg0, current_reg1;
-    current_reg0 = read_byte_data(iic_dev, DEVICE_ADDRESS, reg0);
-    current_reg1 = read_byte_data(iic_dev, DEVICE_ADDRESS, reg1);
+    current_reg0 = read_byte_data(iic_dev, IIC_SLAVE_ADDR, reg0);
+    current_reg1 = read_byte_data(iic_dev, IIC_SLAVE_ADDR, reg1);
     
     tmp = current_reg1 | (current_reg0 & mask);
-    write_byte_data(iic_dev, DEVICE_ADDRESS, reg1, tmp);
-    printf("[DEBUG]: (%x ---> %x)\n", current_reg0, current_reg1, tmp);
+    write_byte_data(iic_dev, IIC_SLAVE_ADDR, reg1, tmp);
+    printf("[DEBUG]: combine %x | (%x & %x) ---> written %x\n", current_reg1, current_reg0, mask, tmp);
 }
 
 void validate_value(XIicPs *iic_dev, u8 reg, u8 mask, u8 val) {
     printf("[DEBUG]: validating value in register %x (desired value %x)\n", reg, val);
-    while(not((read_byte_data(iic_dev, DEVICE_ADDRESS, reg) & mask) == val));
+    while(!((read_byte_data(iic_dev, IIC_SLAVE_ADDR, reg) & mask) == val));
+    printf("ok\n");
 }
 
-void write_config_map(XIicPs *iic_dev, __Reg_Data *map, u32 map_lengh) {
-    //TODO config map
-    for(u32 i=0; i<map_length; ++i) {
-        if(imask == 0xff){
-            write_byte_data(DEVICE_ADDRESS, ireg, ival)
+void write_config_map(XIicPs *iic_dev, __Reg_Data const *map, u32 map_length) {
+    __Reg_Data const *element; 
+    u8 current_reg, combined, clear_cur_val, tmp;
 
-            log.info('reg: {0}, _ => {1} | reg value: {2}'.format(ireg, 
-            ival, bus.read_byte_data(DEVICE_ADDRESS, ireg)))
-        }
-        else {
-            current_reg = bus.read_byte_data(DEVICE_ADDRESS, ireg)
-            clear_cur_val = (current_reg & ~imask)
-            clear_new_val = ival & imask
-            combined = clear_cur_val | clear_new_val
-            bus.write_byte_data(DEVICE_ADDRESS, ireg, combined)
-
-            printf('reg: {0}, {1} => {2} | reg value: {3}'.format(ireg, 
-            current_reg, combined, bus.read_byte_data(DEVICE_ADDRESS, ireg)))
+    for(u32 i=1; i<map_length; ++i) {
+        element = map + i;
+        printf("addr: %i, value: %x, mask: %x\n", element->Reg_Addr, element->Reg_Val, element->Reg_Mask);
+        if(element->Reg_Mask == 0xff) {
+            write_byte_data(iic_dev, IIC_SLAVE_ADDR, element->Reg_Addr, element->Reg_Val);
+            tmp = read_byte_data(iic_dev, IIC_SLAVE_ADDR, element->Reg_Addr);
+            printf("[DEBUG]: addr %i; written %x ---> read %x\n", element->Reg_Addr, element->Reg_Val, tmp);
+        } else {
+            current_reg = read_byte_data(iic_dev, IIC_SLAVE_ADDR, element->Reg_Addr);
+            clear_cur_val = (current_reg & ~element->Reg_Mask);
+            tmp = element->Reg_Val & element->Reg_Mask;
+            combined = clear_cur_val | tmp;
+            write_byte_data(iic_dev, IIC_SLAVE_ADDR, element->Reg_Addr, combined);
+            current_reg = read_byte_data(iic_dev, IIC_SLAVE_ADDR, element->Reg_Addr);
+            printf("[DEBUG]: addr %i; written %x ---> read %x\n", element->Reg_Addr, combined, current_reg);
         }
     }
 }
@@ -171,24 +174,23 @@ int initialize_clock(u16 DeviceId) {
     XIicPs Iic;
 
     if(initialize_iic(DeviceId, &Iic)) { 
-        printf("I2C INITIALIZATION FAILED");
+        printf("I2C INITIALIZATION FAILED\n");
         return XST_FAILURE;
     }
 
-    write_byte_data(&Iic, IIC_SLAVE_ADDR, 0x31, 0x10);
-    printf("Read vaule: %u",read_byte_data(&Iic, IIC_SLAVE_ADDR,  0x31));
+    /* write_byte_data(&Iic, IIC_SLAVE_ADDR, 0x31, 0x10);
+    printf("Read vaule: %u",read_byte_data(&Iic, IIC_SLAVE_ADDR,  0x31)); */
 
-    /* set_bits(&Iic, 230, 0x10) //disable outputs
+    set_bits(&Iic, 230, 0x10); //disable outputs
 
-    printf("lol pause");
+    printf("lol pause\n");
     set_bits(&Iic, 241, 0x80); //pause lol
 
-    printf("writing config");
+    printf("writing config\n");
     write_config_map(&Iic, code_Reg_Store, NUM_REGS_MAX);
 
-    printf("validating input clock ...");
+    printf("validating input clock ... \n");
     validate_value(&Iic, 218, 0x04, 0x00);
-    printf("ok");
 
     clear_bits(&Iic, 49, 0x80); //configure pll for locking
     set_bits(&Iic, 246, 0x02); //initiate locking of ppl
@@ -198,11 +200,10 @@ int initialize_clock(u16 DeviceId) {
     clear_bits(&Iic, 241, 0x80); //restart lol
     set_bits(&Iic, 241, 0x65);
     
-    printf("waiting for locking pll ...");
+    printf("waiting for locking pll ... \n");
     validate_value(&Iic, 218, 0x15, 0x00);
-    printf("ok"&Iic, )//
 
-    printf("copying registers");
+    printf("copying registers\n");
     copy_bits(&Iic, 237, 47, 0x03);
     copy_bits(&Iic, 236, 46, 0xff);
     copy_bits(&Iic, 235, 45, 0xff);
@@ -210,9 +211,9 @@ int initialize_clock(u16 DeviceId) {
     set_bits(&Iic, 47, 0x14);
     set_bits(&Iic, 49, 0x80); //set pll to use fcal
 
-    printf("enabling outputs");
+    printf("enabling outputs\n");
     clear_bits(&Iic, 230, 0x10); //enable outputs
-    printf("done"); */
+    printf("done\n");
 
     return XST_SUCCESS;
 }
