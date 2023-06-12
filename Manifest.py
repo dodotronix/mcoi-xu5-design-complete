98edfe84006428b92e156a96ee2fec0a49b5c61c
Petr Pacner <petr.pacner@cern.ch>
2023-06-12 16:15:13 +0200

    created initial manifests

---
+++ Manifest.py
@@ -0,0 +1,19 @@
+modules = {
+        "git": ["ssh://git@gitlab.cern.ch:7999/vfc_components/ckrs.git::master",
+                "ssh://git@gitlab.cern.ch:7999/vfc_components/mko.git::master",
+                "ssh://git@gitlab.cern.ch:7999/vfc_components/get_edge.git::master",
+                "ssh://git@gitlab.cern.ch:7999/vfc_components/manyff.git::master",
+                "ssh://git@gitlab.cern.ch:7999/personal-digital-lib/zynq_usplus_gbt_fpga.git" 
+                ],
+
+        "local": ["hdl/src/", 
+                  "hdl/constraints", 
+                  "hdl/ip_cores"],
+
+        "system": ["vhdl", "xilinx"]
+        }
+
+fetchto = "libs"
+
+if action == "simulation":
+    modules["local"].append("hdl/tests")
