455bdc0b52d7c0cfb0b73490d719bf330a0a8aa1
Petr Pacner <petr.pacner@cern.ch>
2023-06-19 10:07:57 +0200

    wrong naming of the package and grade

--- syn/Manifest.py
+++ syn/Manifest.py
@@ -2,12 +2,12 @@ target = "xilinx"
 action = "synthesis"
 
 syn_device = "xczu4ev"
-syn_grade = "-1"
-syn_package = "sfvc784"
+syn_grade = "-1-i"
+syn_package = "-sfvc784"
 syn_top = "mcoi_xu5_design_complete"
 syn_project = "mcoi_xu5"
 syn_tool = "vivado"
-# syn_properties = []
+syn_properties = []
 
 include_dirs = ["../hdl/src"]
 
@@ -16,11 +16,11 @@ include_dirs = ["../hdl/src"]
 
 modules = {"local" : ["../"]}
 
-# NOTE hdlmake is going to print some warnings
+# NOTE hdlmake might print some warnings
 # while checking the missing dependencies  
-# the tmp folder is created so when the Makefile
-# is generated, all the tmp content is going to
-# be added
+# the tmp folder is created so when the 
+# Makefile  is generated, all the tmp 
+# content is going to be added
 import os
 if os.path.exists("../tmp"):
     modules['local'].append("../tmp") 
