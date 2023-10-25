cc7274b862fe1329ea5588089ae6cadaefbc3894
Petr Pacner <petr.pacner@cern.ch>
2023-10-25 16:12:06 +0200

    code beautification

--- hdl/src/MCPkg.sv
+++ hdl/src/MCPkg.sv
@@ -27,9 +27,9 @@ package MCPkg;
    // MC registers, add bitwidth of NUMBER_OF_MOTORS_PER_FIBER increased by one
    // because we implement
    parameter MCLINK_WB_ADDRESS_WIDTH =
-				      $clog2(NUMBER_OF_MC_REGISTERS) +
-				      $clog2(NUMBER_OF_MOTORS_PER_FIBER +
-					     NUMBER_OF_LINK_REGISTERS);
+       $clog2(NUMBER_OF_MC_REGISTERS) +
+       $clog2(NUMBER_OF_MOTORS_PER_FIBER +
+       NUMBER_OF_LINK_REGISTERS);
 
    // define number of GBT links to be instantiated
    parameter NUMBER_OF_GBT_LINKS = 4;
@@ -42,10 +42,6 @@ package MCPkg;
    // start to 'forget' IRQs, too largs and we blow FPGA memory. Value
    // of 4 leaves us with 16 samples of 16 bits, so no big deal
    parameter IRQ_GBT_FIFO_BITWIDTH = 4;
-
-
-
-
 
    // these two functions realize division or multiplication of
    // speedIn input by 2**(SPEED_FRACTION). As speedIn is related to
@@ -61,8 +57,8 @@ package MCPkg;
 
    // typedef which declares motor input and output data
    typedef struct packed {
-      logic OH_i;			// overheat
-      logic StepPFail_i;		// stepper controller failure
+      logic OH_i; // overheat
+      logic StepPFail_i; // stepper controller failure
       // input switches - RawSwitches_b2[0] corresponds to 'IN' switch
       // and RawSwitches_b2[1] corresponds to 'OUT' swich. This structure is
       // extensible to 'many' swiches, where 'many' corresponds to maximum of 3
@@ -72,15 +68,15 @@ package MCPkg;
    } mcinput_t;
 
    typedef struct packed {
-      logic StepOutP_o;		// pulse to move stepper motor
-      logic StepDeactivate_o;		// motor stepping enable
-      logic StepBOOST_o;		// motor stepping boost
-      logic StepDIR_o;		// direction of move
+      logic StepOutP_o; // pulse to move stepper motor
+      logic StepDeactivate_o; // motor stepping enable
+      logic StepBOOST_o; // motor stepping boost
+      logic StepDIR_o; // direction of move
    } mcoutput_t;
 
    // typedef defines for each motor switch polarity
-   typedef enum logic {NORMAL = 1'b0,
-		       INVERT = 1'b1} polarity_t;
+   typedef enum logic {NORMAL = 1'b0, INVERT = 1'b1} polarity_t;
+
    // define structure for each output switch
    typedef struct packed {
       // switch is NORMAL or INVERTED?
@@ -92,15 +88,15 @@ package MCPkg;
 
    typedef struct {
       // global external trigger
-      logic 	  TrigMoveExt_i;
+      logic TrigMoveExt_i;
       // global internal trigger - by global register command
-      logic 	  TrigMoveInt_i;
+      logic TrigMoveInt_i;
       // '1' when triggering is enabled
-      logic 	  TriggerEnable_i;
+      logic TriggerEnable_i;
    } triggers_t;
 
    // structure declaring for each _output switch_ the inputs
-   typedef struct 					 {
+   typedef struct {
       switchstate_t [1:0] ExtremitySwitches_b2;
       // declare how many motion steps is allowed for the motor when
       // the switch triggers. These are TWO separate registers because
@@ -108,110 +104,109 @@ package MCPkg;
       // differ. P denotes positive direction (= DIR pin and VME
       // direction register set to '0'), N denotes negative direction
       // (=DIR pin and VME direction registers set to '1')
-      logic [5:0] 					 StepsAfterLimitSwitchP_b6;
-      logic [5:0] 					 StepsAfterLimitSwitchN_b6;
+      logic [5:0] StepsAfterLimitSwitchP_b6;
+      logic [5:0] StepsAfterLimitSwitchN_b6;
       // having given clock, the SlowDown_b32 parameter will slow down the
       // communication process of the MC by factor of SlowDown_b32
       // during the motion of the motor.
-      logic [31:0] 					 SlowDown_b32;
+      logic [31:0] SlowDown_b32;
    } cntparam_t;
 
 
 
    // for triggering we use packed struct so we can easily clear out
    // the triggers.
-   typedef struct 					 packed 					 {
-      logic 						 StartMove;
-      logic 						 StopMove;
-      logic 						 DoQueue;
-      logic 						 ResetCounterPos;
-      logic 						 ResetQueue;
+   typedef struct packed {
+      logic StartMove;
+      logic StopMove;
+      logic DoQueue;
+      logic ResetCounterPos;
+      logic ResetQueue;
    } motorcommand_t;
 
    // structure defining status of the command queue
-   typedef struct packed 					 {
+   typedef struct packed {
       // set to '1' if the queue is filled with samples. In this case
       // any further attempt to write into the queue will be ignored
-      logic 						 QueueFull;
+      logic QueueFull;
       // shows how many items is in the queue. Zero means that queue
       // is empty and input command is just passed to the output
       // command, nonzero identifies number of records. This value can
       // never exceed the queue length
-      logic [6:0] 					 QueueItems_u7;
+      logic [6:0]  QueueItems_u7;
    } queuestatus_t;
 
 
 
    // stepping controller status of the last operation
-   typedef struct 					 packed {
+   typedef struct  packed {
       // which direction motor moved
-      bit 						 Direction;
+      bit  Direction;
       // if the motor was stopped by limiting switch
-      bit 						 StopLimit;
+      bit  StopLimit;
       // if the motor was stopped by stopmove trigger
-      bit 						 StopMove;
+      bit  StopMove;
       // if the motor stopped due to disable of move
-      bit 						 EnableMove;
+      bit  EnableMove;
       // if OUT switch triggered
-      bit 						 SW_N_triggered;
+      bit  SW_N_triggered;
       // if IN switch triggered
-      bit 						 SW_P_triggered;
+      bit  SW_P_triggered;
       // if overheat condition appeared
-      bit 						 OH;
+      bit  OH;
       // if stepper failed
-      bit 						 StepPFail;
+      bit  StepPFail;
    } abortstatus_t;
 
 
    // stepping controller status information
-   typedef struct 					 {
+   typedef struct  {
       // position of the counter in # of steps pulsed out. Can be
       // reset to zero by a command in ITriggerParams_x
-      bit [31:0] 					 PosCounter_b32;
+      bit [31:0]  PosCounter_b32;
       // state of the state machine
-      bit [7:0] 					 State;
+      bit [7:0]  State;
       // how many steps are still to be done to finish the movement
-      bit [31:0] 					 StepsLeft_b32;
+      bit [31:0]  StepsLeft_b32;
       // '1' for single cc when the movement is done
-      bit 						 Done;
-      bit 						 Interrupt;
-      abortstatus_t 					 AbortStatus;
+      bit  Done;
+      bit  Interrupt;
+      abortstatus_t  AbortStatus;
       // '1' during the movement to show that it is busy
-      bit 						 Busy;
+      bit  Busy;
    } stepperstatus_t;
 
    // record containing description of a single motor movement. This
    // is used in combination with command queue. Each command in queue
    // describes one motor command based on this record.
-   typedef struct packed 					 {
+   typedef struct packed  {
       // total number of steps, which are used for this particular cycle
-      logic [31:0] 					 StepNumber_b32;
-      logic 						 Direction;
+      logic [31:0]  StepNumber_b32;
+      logic  Direction;
       // speed of the motor before acceleration and during trailing
-      logic [35:0] 					 LowSpeed_b36;
+      logic [35:0]  LowSpeed_b36;
       // speed of the motor during high speed cruising
-      logic [35:0] 					 HighSpeed_b36;
+      logic [35:0]  HighSpeed_b36;
       // during acceleration or deceleration this parameter sets up
       // additional speed increase/decrease each cycle
-      logic [18:0] 					 AccDeccRate_b19;
+      logic [18:0]  AccDeccRate_b19;
 
-      logic [31:0] 					 Trail_b32;
+      logic [31:0]  Trail_b32;
       // bit 1 = ENABLE INTERNAL TRIGGER
       // bit 0 = ENABLE EXTERNAL TRIGGER
-      struct 						 packed {
-	 logic 						 internal;
-	 logic 						 external;
-      } globaltrigger;
-      logic 						 GenInterrupt;
+      struct  packed {
+          logic  internal;
+          logic  external; } globaltrigger;
+
+      logic  GenInterrupt;
       // bit 2 = STANDBY
       // bit 1 = BOOST
       // bit 0 = ENABLE MOVE
-      struct 						 packed {
-	 logic 						 standby;
-	 logic 						 boost;
-	 logic 						 enable_move;
-      } powerconfig;
-   } command_t;
+      struct  packed {
+          logic  standby;
+          logic  boost;
+          logic  enable_move;
+          } powerconfig; } command_t;
 
     // how many flip-flops will be used in the input signal
     // synchronization chain to avoid metastability
--- hdl/src/types.sv
+++ hdl/src/types.sv
@@ -1,17 +1,15 @@
 package types;
    import MCPkg::*;
 
-
-
    // I2c debugging interface
-   typedef enum logic [7:0]  {IDLE,
-			      START,
-			      ADDRESS,
-			      REGADDRESS,
-			      ACK,
-			      WRITESTOP,
-			      STOP}
-		i2c_state_t;
+   typedef enum logic [7:0]  {
+       IDLE,
+       START,
+       ADDRESS,
+       REGADDRESS,
+       ACK,
+       WRITESTOP,
+       STOP} i2c_state_t;
 
    // GBT data reception/transmission frame
    typedef struct packed {
@@ -24,7 +22,6 @@ package types;
       // and paged memory interface
       logic [15:0] mem_data_b16;
    } t_sfp_stream;
-
 
    typedef mcinput_t[NUMBER_OF_MOTORS_PER_FIBER:1] motorsStatuses_t;
    typedef mcoutput_t[NUMBER_OF_MOTORS_PER_FIBER:1] motorsControls_t;
