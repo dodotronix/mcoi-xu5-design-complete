05e43a719e98e0701a7a064c68c0bfa293bb36c2
Petr Pacner <petr.pacner@cern.ch>
2023-10-25 16:14:19 +0200

    restructuration of top level

--- hdl/src/mcoi_xu5_design_complete.sv
+++ hdl/src/mcoi_xu5_design_complete.sv
@@ -38,87 +38,53 @@ import MCPkg::*;
 import CKRSPkg::*;
 import types::*;
 
-
 module mcoi_xu5_design_complete (//motors
-                                 /* t_motors.producer motors_x,
-                                 //optical interface
-                                 //diagnostics */
-                                 t_gbt.producer gbt_x,
-                                 t_diag.producer diag_x,
-                                 /* //display
-                                 t_display.producer display_x,
-                                 output logic mreset_vadj, */
-                                 // clocks - MGT 120MHz
-                                 input logic         mgt_clk_p,
-                                 input logic         mgt_clk_n,
-                                 output logic [2:0] mled,
-                                 // clocks - MGT derived 50MHz
-                                 // input logic         pl_varclk,
-                                 // localosc 100MHz
-                                 input logic         clk100m_pl_p,
-                                 input logic        clk100m_pl_n
+                                 t_motors.producer motors_x,
+
                                  // SFP interface
+                                 t_gbt.producer gbt_x,
 
-                 // serial interfaces
-                 // t_i2c.endpoint i2c_x,
-                 // input logic         rs485_pl_di,
-                 // output logic        rs485_pl_ro
+                                 //diagnostics
+                                 t_diag.producer diag_x,
+
+                                 //display
+                                 t_display.producer display_x,
+
+                                 output logic mreset_vadj,
+
+                                 // clocks - MGT 120MHz
+                                 input logic mgt_clk_p,
+                                 input logic mgt_clk_n,
+
+                                 output logic [2:0] mled,
+
+                                 // clocks - MGT derived 50MHz
+                                 input logic clk100m_pl_p,
+                                 input logic clk100m_pl_n,
+                                 input logic pl_varclk
+
+                                 // serial interface
+                                 // input logic         rs485_pl_di,
+                                 // output logic        rs485_pl_ro
                                 );
-
-
-   bit [20:0] reset_cntr = 21'd0;
-   logic master_reset;
-   logic Clk120MHz_fromgte4;
-   logic txready, rxready;
-   logic rx_frmclk, tx_frmclk;
-
-   t_clocks clk_tree_x();
-   t_gbt_data gbt_data_x(.ClkRs_ix(clk_tree_x.ClkRs40MHz_ix));
-
-   always_ff @(posedge clk_tree_x.ClkRs100MHz_ix.clk)
-     if (~&reset_cntr)
-       reset_cntr <= reset_cntr + $size(reset_cntr)'(1);
-   always_comb master_reset = ~&reset_cntr;
-
-   // reset synchronization into the respective clock domains
-   vme_reset_sync_and_filter u_100MHz_reset_sync
-     (.rst_ir   (1'b0),
-      .clk_ik   (clk_tree_x.ClkRs100MHz_ix.clk),
-      .cen_ie   (1'b1),
-      .data_i   (master_reset),
-      .data_o   (clk_tree_x.ClkRs100MHz_ix.reset));
-
-   vme_reset_sync_and_filter u_40MHzMGMT_reset_sync
-     (.rst_ir   (1'b0),
-      .clk_ik   (clk_tree_x.ClkRs40MHz_ix.clk),
-      .cen_ie   (1'b1),
-      .data_i   (master_reset),
-      .data_o   (clk_tree_x.ClkRs40MHz_ix.reset));
-
-   /* vme_reset_sync_and_filter u_Var_reset_sync
-     (.rst_ir   (1'b0),
-      .clk_ik   (clk_tree_x.ClkRsVar_ix.clk),
-      .cen_ie   (1'b1),
-      .data_i   (master_reset),
-      .data_o   (clk_tree_x.ClkRsVar_ix.reset)); */
-
-  // *!! because we cannot access internals of t_motors !!
-  /* t_motors_structured motors_structured_x();
-  iface_translator i_iface_translator (.*);
-  */
-
-   logic [31:0] cnt_120mhz;
-   logic tick_120;
-
-   logic [31:0] cnt_100mhz;
-   logic tick_100;
 
    // assign clk_tree_x.ClkRsVar_ix.clk = pl_varclk;
    assign diag_x.test[0] = clk_tree_x.ClkRs40MHz_ix.clk;
    assign diag_x.test[1] = clk_tree_x.ClkRs120MHz_ix.clk;
-   assign diag_x.test[2] = tick_120;
-   assign diag_x.test[3] = tick_100;
+   assign diag_x.test[2] = 1'b1;
+   assign diag_x.test[3] = 1'b1;
    assign diag_x.test[4] = 1'b1;
+
+   assign mled[0] = 1'b0;
+   assign mled[1] = 1'b0;
+   assign mled[2] = 1'b0;
+
+   t_clocks clk_tree_x();
+   t_gbt_data gbt_data_x(.ClkRs_ix(clk_tree_x.ClkRs40MHz_ix));
+
+   /* logic [31:0] cnt_120mhz;
+   logic [31:0] cnt_100mhz;
+   logic tick_120, tick_100;
 
    // HELLO WORLD WITH LED
    always_ff@(posedge clk_tree_x.ClkRs100MHz_ix.clk) begin
@@ -139,49 +105,8 @@ module mcoi_xu5_design_complete (//motors
    end
 
    assign mled[1] = tick_120;
-
-   //logic system part
-   /* McoiXu5System i_mcoi_xu5_system (
-       .gbt_los(gbt_x.sfp1_los), .*); */
-
-   // ps part just for storing data to qspi
-   zynq_ultrasp_ps_system i_ps_system();
-
-   // clock generation
-   // 100MHz oscillator and associated reset
-   IBUFDS ibufds_i(
-      .O(clk_tree_x.ClkRs100MHz_ix.clk),
-      .I(clk100m_pl_p),
-      .IB(clk100m_pl_n));
-
-  logic ExternalPll120MHzMGT;
-  // 120MHz coming from MGT oscillator
-  IBUFDS_GTE4 #(.REFCLK_EN_TX_PATH(1'b0),
-      .REFCLK_HROW_CK_SEL(2'b00),
-      .REFCLK_ICNTL_RX(2'b00))
-  ibufds_gte4_i (
-      .O(ExternalPll120MHzMGT),
-      .ODIV2(Clk120MHz_fromgte4),
-      .CEB(1'b0),
-      .I(mgt_clk_p),
-      .IB(mgt_clk_n));
-
-  // 120MHz PLL buffer clock copier
-  BUFG_GT ibuf_txpll_i (
-      .O(clk_tree_x.ClkRs120MHz_ix.clk),
-      .CE(1'b1),
-      .CEMASK(1'b0),
-      .CLR(1'b0),
-      .CLRMASK(1'b0),
-      .DIV(3'b000),
-      .I(Clk120MHz_fromgte4));
-
-  // 40MHz PLL derived from MGT clock
-  gbt_pll_clk40m gbt_pll40m_i (
-      .clk120m_i(clk_tree_x.ClkRs120MHz_ix.clk),
-      .clk40m_o(clk_tree_x.ClkRs40MHz_ix.clk),
-      .reset(0),
-      .locked());
+   assign mled[2] = | gbt_data_x.data_received;
+   assign mreset_vadj = 1'b0;
 
     logic [31:0] dynamic_data;
     always_ff @(posedge gbt_data_x.tx_frameclk) begin
@@ -194,13 +119,21 @@ module mcoi_xu5_design_complete (//motors
         end
     end
 
-    assign gbt_data_x.data_sent.motor_data_b64 = {dynamic_data, dynamic_data};
+    assign gbt_data_x.data_sent.motor_data_b64 = {dynamic_data, dynamic_data}; */
+
+    // in the system you find just buffers plls
+    // and sync of resets with clock domains
+    mcoi_xu5_system sys_i (.*);
+
+    // application serving the stepper motors
+    mcoi_xu5_application app_i (.*);
 
     // GBT instance
     gbt_zynq_usplus #(.DEBUG(1)) gbt_zynq_usplus_inst(
         .external_pll_source_120mhz(ExternalPll120MHzMGT),
         .*);
 
-    assign mled[2] = | gbt_data_x.data_received;
+   // ps part just for storing data to qspi
+   zynq_ultrasp_ps_system i_ps_system();
 
 endmodule // mcoi_xu5_design_complete
