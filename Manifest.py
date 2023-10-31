0a9260ba5c17d3d09366da69257f5b4344fc9330
Petr Pacner <petr.pacner@cern.ch>
2023-10-31 16:35:29 +0100

    module leds added to the diag interface

--- Manifest.py
+++ Manifest.py
@@ -5,13 +5,17 @@ modules = {
                 "ssh://git@gitlab.cern.ch:7999/vfc_components/manyff.git@@256e16feaa17edce335b1a0305cd520dd511f96b",
                 "ssh://git@gitlab.cern.ch:7999/vfc_components/gbt-fpga.git@@8a048686d53e16268d35d85ce15e412655a3a707",
                 "ssh://git@gitlab.cern.ch:7999/vfc_components/serdes.git::master",
+                "ssh://git@gitlab.cern.ch:7999/vfc_components/codec_8b10b.git::master",
+                "ssh://git@gitlab.cern.ch:7999/vfc_components/generic_dpram.git::master",
+                "ssh://git@gitlab.cern.ch:7999/vfc_components/gray_fifo.git::master",
                 "ssh://git@gitlab.cern.ch:7999/vfc_components/memory_transport.git::master",
                 "ssh://git@gitlab.cern.ch:7999/vfc_components/glitch_filter.git::master",
                 "ssh://git@gitlab.cern.ch:7999/vfc_components/cyclic_redundancy_check.git::master",
                 "ssh://git@gitlab.cern.ch:7999/vfc_components/heart_beat.git::master",
                 "ssh://git@gitlab.cern.ch:7999/vfc_components/pwm::master",
                 "ssh://git@gitlab.cern.ch:7999/vfc_components/scrambler::master",
-                "ssh://git@gitlab.cern.ch:7999/vfc_components/tlc5920::master"
+                "ssh://git@gitlab.cern.ch:7999/vfc_components/tlc5920::master",
+                "ssh://git@gitlab.cern.ch:7999/vfc_components/clock_divider::master"
                 ],
 
         "local": ["hdl/src/"],
