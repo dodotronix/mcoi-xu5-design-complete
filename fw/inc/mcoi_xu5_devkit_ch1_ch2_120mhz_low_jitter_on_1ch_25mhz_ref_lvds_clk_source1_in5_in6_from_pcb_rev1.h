2c071aa3c14c307a6389667cde2cbaf8e45038a0
Petr Pacner <petr.pacner@cern.ch>
2024-02-09 09:20:07 +0100

    renamed the generated pll register map to reg_map

--- fw/inc/mcoi_xu5_devkit_ch1_ch2_120mhz_low_jitter_on_1ch_25mhz_ref_lvds_clk_source1_in5_in6_from_pcb_rev1.h
+++ fw/inc/mcoi_xu5_devkit_ch1_ch2_120mhz_low_jitter_on_1ch_25mhz_ref_lvds_clk_source1_in5_in6_from_pcb_rev1.h
@@ -80,7 +80,7 @@ typedef struct Reg_Data{
    unsigned char Reg_Mask;
 } t_reg_data;
 
-t_reg_data const code_Reg_Store[NUM_REGS_MAX] = {
+t_reg_data const reg_map[NUM_REGS_MAX] = {
 {  0,0x00,0x00},
 {  1,0x00,0x00},
 {  2,0x00,0x00},
