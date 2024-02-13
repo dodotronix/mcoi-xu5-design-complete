c0b216840bb9aae213cdfcddd3e46dd4b38d7e00
Petr Pacner <petr.pacner@cern.ch>
2024-02-13 07:57:00 +0100

    reading and printing serial number of the at24mac402

--- fw/inc/at24mac402.h
+++ fw/inc/at24mac402.h
@@ -3,10 +3,25 @@
 
 /* datasheet: https://ww1.microchip.com/downloads/aemDocuments/documents/MPD/ProductDocuments/DataSheets/I2C-Compatible-%28Two-Wire%29-Serial-EEPROM-with-a-Factory-Programmed-EUI-48-or-EUI-64-Address-Plus-a-Unique-Factory-Programmed-128-Bit-Serial-Number-2-Kbit-%28256x8%29-20006430A.pdf */
 
-int at24mac402_init(u8 DeviceAddr);
-int at24mac402_write(u8 reg, u8 val);
-int at24mac402_read(u8 reg);
-int at24mac402_get_EUI64(u8 mac[6]);
-int at24mac402_get_UUID(u8 uuid[16]);
+#include "i2cbus.h"
+
+#define AT24MAC402_ADDR_EEPROM 0x54
+#define AT24MAC402_ADDR_SN_UUID 0x5C
+#define AT24MAC402_ADDR_PROTECTED 0x34
+
+#define AT24MAC402_SN_START 0x80
+#define AT24MAC402_MAC48_START 0x9A
+#define AT24MAC402_MAC64_START 0x98
+
+typedef struct {
+    u8 mac[6];
+    u8 uuid[16];
+    u8 data[4];
+} eeprom_t;
+
+int at24mac402_init(i2c_t *i2c);
+
+void at24mac402_get_mac();
+void at24mac402_get_uuid();
 
 #endif /* AT24MAC402_H_ */
--- fw/src/at24mac402.c
+++ fw/src/at24mac402.c
@@ -1,25 +1,42 @@
 
 #include "at24mac402.h"
 
-int at24mac402_init(u8 DeviceAddr){
+static i2c_t atmac402_bus;
+static eeprom_t mem;
+
+int at24mac402_init(i2c_t *i2c){
+    i2cbus_create_copy(&atmac402_bus, i2c);
+    atmac402_bus.data = mem.mac;
+    atmac402_bus.addr = AT24MAC402_ADDR_SN_UUID;
+    atmac402_bus.writes = 1;
+    atmac402_bus.reads = 6;
+    atmac402_bus.data[0] = AT24MAC402_MAC64_START;
+    
+    if(i2cbus_read_data(&atmac402_bus)) return 1; 
+
+    atmac402_bus.data = mem.uuid;
+    atmac402_bus.writes = 1;
+    atmac402_bus.reads = 16;
+    atmac402_bus.data[0] = AT24MAC402_SN_START;
+
+    if(i2cbus_read_data(&atmac402_bus)) return 1; 
+    atmac402_bus.data = mem.data;
     return 0;
 }
 
-int at24mac402_write(u8 reg, u8 val){
-    return 0;
-}
- 
-int at24mac402_read(u8 reg){
-    return 0;
-}
+static void display(u8 *data, u8 len) {
+    for(int i=0; i<len; ++i){
+        printf("%02x ", data[i]);
+    }
+    printf("\n");
+};
 
-int at24mac402_get_EUI64(u8 mac[6]){
-    return 0;
-}
+void at24mac402_get_mac() {
+    printf("MAC: ");
+    display(mem.mac, 6);
+};
 
-int at24mac402_get_UUID(u8 uuid[16]){
-    return 0;
-}
-
-
-
+void at24mac402_get_uuid() {
+    printf("UUID: ");
+    display(mem.uuid, 16);
+};
