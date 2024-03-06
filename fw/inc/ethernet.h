9597644faad4b86d5873cee6caac82df661df648
Petr Pacner <petr.pacner@cern.ch>
2024-03-06 11:23:38 +0100

    copy-pasted ethernet echo in one directory from generated codes

---
+++ fw/inc/ethernet.h
@@ -0,0 +1,34 @@
+#ifndef ETHERNET_H_ 
+#define ETHERNET_H_
+
+#include <string.h>
+
+#include "platform_config.h"
+#include "xil_printf.h"
+#include "netif/xadapter.h"
+#include "lwip/sockets.h"
+#include "netif/xadapter.h"
+#include "lwipopts.h"
+#include "lwip/init.h"
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#if LWIP_IPV6==1
+#include "lwip/ip.h"
+#else
+#if LWIP_DHCP==1
+#include "lwip/dhcp.h"
+#endif
+#endif
+
+#define MAX_CONNECTIONS 8
+#define THREAD_STACKSIZE 1024
+
+int main_thread();
+void print_echo_app_header();
+void echo_application_thread();
+void process_echo_request(void *p);
+void print_echo_app_header();
+
+#endif /* ETHERNET_H_ */
---
+++ fw/src/ethernet.c
@@ -0,0 +1,307 @@
+
+#include "ethernet.h"
+
+u16_t echo_port = 7;
+int new_sd[MAX_CONNECTIONS];
+int connection_index;
+
+#if LWIP_IPV6==0
+#if LWIP_DHCP==1
+extern volatile int dhcp_timoutcntr;
+err_t dhcp_start(struct netif *netif);
+#endif
+#endif
+
+static struct netif server_netif;
+struct netif *echo_netif;
+
+#if LWIP_IPV6==1
+void print_ip6(char *msg, ip_addr_t *ip)
+{
+	print(msg);
+	xil_printf(" %x:%x:%x:%x:%x:%x:%x:%x\n\r",
+			IP6_ADDR_BLOCK1(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK2(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK3(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK4(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK5(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK6(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK7(&ip->u_addr.ip6),
+			IP6_ADDR_BLOCK8(&ip->u_addr.ip6));
+}
+
+#else
+void print_ip(char *msg, ip_addr_t *ip)
+{
+	xil_printf(msg);
+	xil_printf("%d.%d.%d.%d\n\r", ip4_addr1(ip), ip4_addr2(ip),
+			ip4_addr3(ip), ip4_addr4(ip));
+}
+
+void print_ip_settings(ip_addr_t *ip, ip_addr_t *mask, ip_addr_t *gw)
+{
+	print_ip("Board IP: ", ip);
+	print_ip("Netmask : ", mask);
+	print_ip("Gateway : ", gw);
+}
+#endif
+
+
+void network_thread(void *p)
+{
+    struct netif *netif;
+    /* the mac address of the board. this should be unique per board */
+    unsigned char mac_ethernet_address[] = { 0x00, 0x0a, 0x35, 0x00, 0x01, 0x02 };
+#if LWIP_IPV6==0
+    ip_addr_t ipaddr, netmask, gw;
+#if LWIP_DHCP==1
+    int mscnt = 0;
+#endif
+#endif
+
+    netif = &server_netif;
+
+    xil_printf("\r\n\r\n");
+    xil_printf("-----lwIP Socket Mode Echo server Demo Application ------\r\n");
+
+#if LWIP_IPV6==0
+#if LWIP_DHCP==0
+    /* initialize IP addresses to be used */
+    IP4_ADDR(&ipaddr,  192, 168, 1, 10);
+    IP4_ADDR(&netmask, 255, 255, 255,  0);
+    IP4_ADDR(&gw,      192, 168, 1, 1);
+#endif
+
+    /* print out IP settings of the board */
+
+#if LWIP_DHCP==0
+    print_ip_settings(&ipaddr, &netmask, &gw);
+    /* print all application headers */
+#endif
+
+#if LWIP_DHCP==1
+	ipaddr.addr = 0;
+	gw.addr = 0;
+	netmask.addr = 0;
+#endif
+#endif
+
+#if LWIP_IPV6==0
+    /* Add network interface to the netif_list, and set it as default */
+    if (!xemac_add(netif, &ipaddr, &netmask, &gw, mac_ethernet_address, PLATFORM_EMAC_BASEADDR)) {
+	xil_printf("Error adding N/W interface\r\n");
+	return;
+    }
+#else
+    /* Add network interface to the netif_list, and set it as default */
+    if (!xemac_add(netif, NULL, NULL, NULL, mac_ethernet_address, PLATFORM_EMAC_BASEADDR)) {
+	xil_printf("Error adding N/W interface\r\n");
+	return;
+    }
+
+    netif->ip6_autoconfig_enabled = 1;
+
+    netif_create_ip6_linklocal_address(netif, 1);
+    netif_ip6_addr_set_state(netif, 0, IP6_ADDR_VALID);
+
+    print_ip6("\n\rBoard IPv6 address ", &netif->ip6_addr[0].u_addr.ip6);
+#endif
+
+    netif_set_default(netif);
+
+    /* specify that the network if is up */
+    netif_set_up(netif);
+
+    /* start packet receive thread - required for lwIP operation */
+    sys_thread_new("xemacif_input_thread", (void(*)(void*))xemacif_input_thread, netif,
+            THREAD_STACKSIZE,
+            DEFAULT_THREAD_PRIO);
+
+#if LWIP_IPV6==0
+#if LWIP_DHCP==1
+    dhcp_start(netif);
+    while (1) {
+		vTaskDelay(DHCP_FINE_TIMER_MSECS / portTICK_RATE_MS);
+		dhcp_fine_tmr();
+		mscnt += DHCP_FINE_TIMER_MSECS;
+		if (mscnt >= DHCP_COARSE_TIMER_SECS*1000) {
+			dhcp_coarse_tmr();
+			mscnt = 0;
+		}
+	}
+#else
+    xil_printf("\r\n");
+    xil_printf("%20s %6s %s\r\n", "Server", "Port", "Connect With..");
+    xil_printf("%20s %6s %s\r\n", "--------------------", "------", "--------------------");
+
+    print_echo_app_header();
+    xil_printf("\r\n");
+    sys_thread_new("echod", echo_application_thread, 0,
+		THREAD_STACKSIZE,
+		DEFAULT_THREAD_PRIO);
+    vTaskDelete(NULL);
+#endif
+#else
+    print_echo_app_header();
+    xil_printf("\r\n");
+    sys_thread_new("echod",echo_application_thread, 0,
+		THREAD_STACKSIZE,
+		DEFAULT_THREAD_PRIO);
+    vTaskDelete(NULL);
+#endif
+    return;
+}
+
+
+int main_thread()
+{
+#if LWIP_DHCP==1
+	int mscnt = 0;
+#endif
+
+	/* initialize lwIP before calling sys_thread_new */
+    lwip_init();
+
+    /* any thread using lwIP should be created using sys_thread_new */
+    sys_thread_new("NW_THRD", network_thread, NULL,
+		THREAD_STACKSIZE,
+            DEFAULT_THREAD_PRIO);
+
+#if LWIP_IPV6==0
+#if LWIP_DHCP==1
+    while (1) {
+	vTaskDelay(DHCP_FINE_TIMER_MSECS / portTICK_RATE_MS);
+		if (server_netif.ip_addr.addr) {
+			xil_printf("DHCP request success\r\n");
+			print_ip_settings(&(server_netif.ip_addr), &(server_netif.netmask), &(server_netif.gw));
+			print_echo_app_header();
+			xil_printf("\r\n");
+			sys_thread_new("echod", echo_application_thread, 0,
+					THREAD_STACKSIZE,
+					DEFAULT_THREAD_PRIO);
+			break;
+		}
+		mscnt += DHCP_FINE_TIMER_MSECS;
+		if (mscnt >= DHCP_COARSE_TIMER_SECS * 2000) {
+			xil_printf("ERROR: DHCP request timed out\r\n");
+			xil_printf("Configuring default IP of 192.168.1.10\r\n");
+			IP4_ADDR(&(server_netif.ip_addr),  192, 168, 1, 10);
+			IP4_ADDR(&(server_netif.netmask), 255, 255, 255,  0);
+			IP4_ADDR(&(server_netif.gw),  192, 168, 1, 1);
+			print_ip_settings(&(server_netif.ip_addr), &(server_netif.netmask), &(server_netif.gw));
+			/* print all application headers */
+			xil_printf("\r\n");
+			xil_printf("%20s %6s %s\r\n", "Server", "Port", "Connect With..");
+			xil_printf("%20s %6s %s\r\n", "--------------------", "------", "--------------------");
+
+			print_echo_app_header();
+			xil_printf("\r\n");
+			sys_thread_new("echod", echo_application_thread, 0,
+					THREAD_STACKSIZE,
+					DEFAULT_THREAD_PRIO);
+			break;
+		}
+	}
+#endif
+#endif
+    vTaskDelete(NULL);
+    return 0;
+}
+
+/* thread spawned for each connection */
+void process_echo_request(void *p)
+{
+	int sd = *(int *)p;
+	int RECV_BUF_SIZE = 2048;
+	char recv_buf[RECV_BUF_SIZE];
+	int n, nwrote;
+
+	while (1) {
+		/* read a max of RECV_BUF_SIZE bytes from socket */
+		if ((n = read(sd, recv_buf, RECV_BUF_SIZE)) < 0) {
+			xil_printf("%s: error reading from socket %d, closing socket\r\n", __FUNCTION__, sd);
+			break;
+		}
+
+		/* break if the recved message = "quit" */
+		if (!strncmp(recv_buf, "quit", 4))
+			break;
+
+		/* break if client closed connection */
+		if (n <= 0)
+			break;
+
+		/* handle request */
+		if ((nwrote = write(sd, recv_buf, n)) < 0) {
+			xil_printf("%s: ERROR responding to client echo request. received = %d, written = %d\r\n",
+					__FUNCTION__, n, nwrote);
+			xil_printf("Closing socket %d\r\n", sd);
+			break;
+		}
+	}
+
+	/* close connection */
+	close(sd);
+	vTaskDelete(NULL);
+}
+
+
+void echo_application_thread()
+{
+	int sock;
+	int size;
+#if LWIP_IPV6==0
+	struct sockaddr_in address, remote;
+
+	memset(&address, 0, sizeof(address));
+
+	if ((sock = lwip_socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		return;
+
+	address.sin_family = AF_INET;
+	address.sin_port = htons(echo_port);
+	address.sin_addr.s_addr = INADDR_ANY;
+#else
+	struct sockaddr_in6 address, remote;
+
+	memset(&address, 0, sizeof(address));
+
+	address.sin6_len = sizeof(address);
+	address.sin6_family = AF_INET6;
+	address.sin6_port = htons(echo_port);
+
+	memset(&(address.sin6_addr), 0, sizeof(address.sin6_addr));
+
+	if ((sock = lwip_socket(AF_INET6, SOCK_STREAM, 0)) < 0)
+		return;
+#endif
+
+	if (lwip_bind(sock, (struct sockaddr *)&address, sizeof (address)) < 0)
+		return;
+
+	lwip_listen(sock, 0);
+
+	size = sizeof(remote);
+
+	while (1) {
+		if ((new_sd[connection_index] = lwip_accept(sock, (struct sockaddr *)&remote, (socklen_t *)&size)) > 0) {
+			sys_thread_new("echos", process_echo_request,
+				(void*)&(new_sd[connection_index]),
+				THREAD_STACKSIZE,
+				DEFAULT_THREAD_PRIO);
+			if (++connection_index>= MAX_CONNECTIONS) {
+				break;
+			}
+		}
+	}
+	xil_printf("Maximum number of connections reached, No further connections will be accepted\r\n");
+	vTaskSuspend(NULL);
+}
+
+void print_echo_app_header()
+{
+    xil_printf("%20s %6d %s\r\n", "echo server",
+                        echo_port,
+                        "$ telnet <board_ip> 7");
+
+}
