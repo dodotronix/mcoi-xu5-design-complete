ca81b5b272915e59f317954612efadf21f8d125c
Petr Pacner <petr.pacner@cern.ch>
2024-03-06 11:26:30 +0100

    test if control signal will arrive to ps

--- hdl/src/mcoi_xu5_system.sv
+++ hdl/src/mcoi_xu5_system.sv
@@ -170,6 +170,7 @@ module mcoi_xu5_system (
         reset = gbt_data_x.los | !ext_pll_ready;
         global_reset = rst_pulse | gbt_data_x.los;
         clk_tree_x.ClkRsVar_ix.clk = recovered_clock;
+        ps_register_x.control = 32'hcafebabe;
     end
 
     // reset synchronization into the respective clock domains
