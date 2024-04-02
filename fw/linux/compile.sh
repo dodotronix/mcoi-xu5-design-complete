3bc76d5146d05cfa2cfdcad64913ccaf480d72a0
Petr Pacner <petr.pacner@cern.ch>
2024-04-02 20:13:22 +0200

    automation of the petalinux build process

--- fw/linux/compile.sh
+++ fw/linux/compile.sh
@@ -1,10 +1,41 @@
 #!/usr/bin/bash
+#
+# Petalinux commands:
+# petalinux-create -t <target> -s <script>
+# or
+# petalinux-create -t project --template zynqmp --name McoiCore
+# petalinux-config -get-hw-description=<path_to_xsa>
+#
+# petalinux-build
+#
+# This opens a configuration menu
+# petalinux-config
+# NOTE: If you want test your build in QEMU use the initramfs filesystem
+#
+# copies binaries to the pre-built folder
+# petalinux-package --prebuilt
+#
+# Petalinux Qemu simulations:
+# petalinux-boot --qemu --prebuilt 3
+#
+# For SD card we have to create BOOT.BIN file and then wic file
+# petalinux-package --boot --format BIN --fsbl images/linux/zynqmp_fsbl.elf \
+# --u-boot images/linux/u-boot.elf --pmufw images/linux/pmu_rom_qemu_sha3.elf --force
+#
+# If you want to simulate sd card you need to define the structure of the
+# memory using the wic file
+# petalinux-package --wic
+#
+# Creating an app which is automatically enabled
+# petalinux-create -t apps --template install --name McoiCore --enable
 
 source /opt/petalinux/settings.sh
-echo $PATH "\n"
 
-petalinux-create -t project -s *.bsp 
-ls -l
+petalinux-create -t project --template zynqMP --name McoiXu5BSP  
+cd McoiXu5BSP
+petalinux-config --get-hw-description=../exported_hw.xsa
 
-cd ME-XU5-4EV-1I-D11E_PE1_QSPI
-petalinux-build
+cp -R ../petalinux_spec/ .
+petalinux-config --silentconfig
+
+#petalinux-build
---
+++ fw/linux/petalinux_spec/project-spec/meta-user/recipes-apps/mcoiapp/files/mcoicore.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <linux/i2c-dev.h>
+#include <sys/mman.h>
+
+int main(int argc, char **argv)
+{
+    printf("Doprdele prace :DDD!\n");
+    return 0;
+}   
